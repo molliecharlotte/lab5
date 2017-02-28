47b129845e6e7fcf4a7a022e0f91e7c734adc25a
Mol <molliecharlottesuss@gmail.com>
2017-02-28 12:21:58 -0500

    Add files via upload

---
+++ InsertionSort.java
@@ -0,0 +1,33 @@
+package lab5;
+
+public class InsertionSort<T> implements ISorting<T>
+{
+	public InsertionSort()
+	{
+		
+	}
+
+	@Override
+	public Comparable[] sort(Comparable[] elements) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Comparable[] descendingSort(Comparable[] elements) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public T[] sort(T[] elements) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public T[] descendingSort(T[] elements) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
